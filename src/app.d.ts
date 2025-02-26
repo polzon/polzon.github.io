cdf9e3020185adad1db4b2ff89153dcee61c6144
Zack Polson <zack@polson.dev>
2025-02-26 16:29:08 -0700

    pls work with github pages

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+export const prerender = true;
+
 declare global {
 	namespace App {
 		// interface Error {}
