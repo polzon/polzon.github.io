7748c146f4490de4adbbe35ed6848bc0ffed8e3f
Zack Polson <zack@polson.dev>
2025-02-28 01:57:22 -0700

    q

--- svelte.config.js
+++ svelte.config.js
@@ -19,9 +19,9 @@ const config = {
 			precompress: true,
 			strict: true
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	}
 };
 
