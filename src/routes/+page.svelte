72e01dd6f54126fe084f49bc3f5bb305f9a647c2
Zack Polson <zack@polson.dev>
2025-02-28 02:55:57 -0700

    new seo tweaks and content update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,26 +7,30 @@
 <div>
     <p>
       <b>About Me:</b><br />
-      This is my personal website hosted on Debian. This site shows off some
-      things I'm working on, contact information, and some resources I want to
-      share. It is frequently updated more as I work on various projects and learn
-      more about web development.
+      This is my personal website hosted on Github Pages using Svelte.
+      This site shows off some things I'm working on, contact information,
+      and some resources I want to share. It is frequently updated more as
+      I work on various projects and learn more about web development.
+    </p>
+    <p>
+      Currently overhauling this website to show-off my new projects, and as
+      I learn how to properly use Svelte. More of my game progress to come soon!
     </p>
   </div>
   <p>
     For some of my work,
-    <a href="https://github.com/polzon/">check out my GitHub.</a><br />
+    <a href="https://github.com/polzon/">check out my GitHub</a>.<br />
   </p>
   <p>
     <b>Resources:</b><br />
-    I'm working on an unnamed game, check out a public web build
-    <a href="https://shipp.dev/demo">here</a>.<br />
-    You can check out some of my browser research on
-    <a href="https://shipp.dev/fs.php">this page</a>.<br />
+    I'm working on an unnamed game, check out my
+    <a href="https://shipp.dev/demo">public web build</a>.<br />
+    You can check out some of my
+    <a href="https://shipp.dev/fs.php">browser fingerprint research</a>.<br />
   </p>
   <p>
     <b>Contact:</b><br />
-    Email: <a href="mailto:public@polson.dev">public@polson.dev</a>
+    Email: <a href="mailto:zack@polson.dev">zack@polson.dev</a>
     (<a href="https://shipp.dev/pubkey.txt">pgp</a>)<br />
     Bsky: <a href="https://bsky.app/profile/polson.dev">@polson.dev</a><br />
   </p>
