c8a26ab3e86df7483c17319d3e9963b8645829f6
Zack Polson <zack@polson.dev>
2026-03-26 21:54:57 +0000

    wip
    Signed-off-by: Zack Polson <zack@polson.dev>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
+  // turbopack: {},
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
