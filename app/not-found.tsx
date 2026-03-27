44017dd4888f8bd49342944bd1a28a3b705b9b71
Zack Polson <zack@polson.dev>
2026-03-27 01:51:56 -0600

    added a custom 404 page
    Signed-off-by: Zack Polson <zack@polson.dev>

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+export default function NotFound() {
+  return (
+    <main>
+      <div>
+        <p>
+          <b>404:</b>
+          <br />
+          Page could not be found. What are you doing here?
+        </p>
+      </div>
+
+      <div>
+        <p>
+          <a href="/">Return home</a>
+        </p>
+      </div>
+    </main>
+  );
+}
