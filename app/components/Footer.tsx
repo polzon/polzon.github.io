5de1fbf18918520807b4a8e5c9bf0d8ec8c27b06
Zack Polson <zack@polson.dev>
2026-03-27 00:31:54 -0600

    configured linting and removed some unused stuff
    Signed-off-by: Zack Polson <zack@polson.dev>

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 export default function Footer() {
-    return (
-        <div>
-            <footer>
-                <p>
-                    <em>
-                        Thanks for visiting,
-                        <br />
-                        - Zack Polson
-                    </em>
-                </p>
-            </footer>
-        </div>
-    )
+  return (
+    <div>
+      <footer>
+        <p>
+          <em>
+            Thanks for visiting,
+            <br />- Zack Polson
+          </em>
+        </p>
+      </footer>
+    </div>
+  );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,31 +1,31 @@
 "use client";
 
 const NAV_LINKS = [
-    { label: "Home", href: "/" },
-    { label: "Blog", href: "/" },
-    { label: "Play", href: "/" },
-    { label: "About", href: "/" },
-    { label: "Contact", href: "/" },
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/" },
+  { label: "Play", href: "/" },
+  { label: "About", href: "/" },
+  { label: "Contact", href: "/" },
 ];
 
 export default function Header() {
-    return (
-        <header>
-            <div style={{ textAlign: "center" }}>
-                <h1>
-                    <b>Zack Polson</b>
-                </h1>
-                <nav>
-                    {NAV_LINKS.map((link, i) => (
-                        <span key={link.label}>
-                            <a href={link.href}>{link.label}</a>
-                            {i < NAV_LINKS.length - 1 && " | "}
-                        </span>
-                    ))}
-                </nav>
-                <p />
-            </div>
-            <hr />
-        </header>
-    );
+  return (
+    <header>
+      <div style={{ textAlign: "center" }}>
+        <h1>
+          <b>Zack Polson</b>
+        </h1>
+        <nav>
+          {NAV_LINKS.map((link, i) => (
+            <span key={link.label}>
+              <a href={link.href}>{link.label}</a>
+              {i < NAV_LINKS.length - 1 && " | "}
+            </span>
+          ))}
+        </nav>
+        <p />
+      </div>
+      <hr />
+    </header>
+  );
 }
