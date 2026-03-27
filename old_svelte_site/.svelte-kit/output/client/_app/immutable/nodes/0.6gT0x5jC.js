40e733ee94f1a17d778a19a487010b50d6d7e12f
Zack Polson <zack@polson.dev>
2026-03-26 18:28:27 -0600

    clearing old files
    Signed-off-by: Zack Polson <zack@polson.dev>

--- old_svelte_site/.svelte-kit/output/client/_app/immutable/nodes/0.6gT0x5jC.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import{c as n,f as m,a as p}from"../chunks/Bz-HT6m8.js";import{s as e}from"../chunks/0mipC0A-.js";function f(a,r){var o=n(),t=m(o);e(t,()=>r.children),p(a,o)}export{f as component};
--- old_svelte_site/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.6gT0x5jC.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/Bz-HT6m8.js","_app/immutable/chunks/0mipC0A-.js"];
-export const stylesheets = [];
-export const fonts = [];
