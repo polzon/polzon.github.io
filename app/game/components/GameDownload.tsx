510018720790db75c4f51c81e2d6fda675471a91
Zack Polson <zack@polson.dev>
2026-03-30 06:47:37 -0600

    now detects architecture and OS
    Signed-off-by: Zack Polson <zack@polson.dev>

--- app/game/components/GameDownload.tsx
+++ app/game/components/GameDownload.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { UAParser } from "ua-parser-js";
+
 export default function GameDownload() {
+  const [systemInfo, setSystemInfo] = useState("Detecting...");
+
+  useEffect(() => {
+    const parser = new UAParser(window.navigator.userAgent);
+    const cpu = parser.getCPU();
+    const os = parser.getOS();
+    const osLabel = [os.name ?? "Unknown OS", os.version]
+      .filter(Boolean)
+      .join(" ");
+    setSystemInfo(
+      `${osLabel ?? "Unknown OS"} (${cpu.architecture ?? "Unknown CPU"})`,
+    );
+  }, []);
+
   return (
-    <a href="https://polzon.itch.io/quintessence/download/w0CQSfXMkbPT6nRc28k_WK3ncEJ0GL03nixz5F6a">
-      Direct Download
-    </a>
+    <div>
+      {" "}
+      <p>Your system: {systemInfo}</p>
+      <a href="https://polzon.itch.io/quintessence/download/w0CQSfXMkbPT6nRc28k_WK3ncEJ0GL03nixz5F6a">
+        Direct Download
+      </a>
+    </div>
   );
 }
